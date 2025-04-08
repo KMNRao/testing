4c259071c0e6a9d45570f89a452da43296297751
KMNRao <kmohanarao09@gmail.com>
2025-04-08 16:33:18 +0530

    commit

---
+++ 01-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1 : how r u?"
+echo "$person2 : how r u?"
+echo "$person1 : are you practicing git?"
+echo "$person2 : yes i am practicing?"
+echo "$person1 : what about you?"
