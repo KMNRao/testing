5c569a95efd077aad439a9508e2406d56aaaab59
KMNRao <kmohanarao09@gmail.com>
2025-04-08 16:21:39 +0530

    commit

--- shell.sh
+++ shell.sh
@@ -1,3 +1,16 @@
-Hi Here i am practicing shell script
+#!/bin/bsh
 
-here i am practicing git also
+person1=ramesh
+person2=suresh
+
+echo "ramesh : how r u?"
+echo "suresh : how r u?"
+echo "ramesh : are you practicing git?"
+echo "suresh : yes i am practicing?"
+echo "ramesh : what about you?"
+
+echo "$person1 : how r u?"
+echo "$person2 : how r u?"
+echo "$person1 : are you practicing git?"
+echo "$person2 : yes i am practicing?"
+echo "$person1 : what about you?"
