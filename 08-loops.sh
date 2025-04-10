f9a2ca46d3f5896995fd4b1760044a9b37d273ef
KMNRao <kmohanarao09@gmail.com>
2025-04-10 15:55:01 +0530

    commit

--- 08-loops.sh
+++ 08-loops.sh
@@ -8,9 +8,12 @@ Y="\e[33m"
 N="\e[0m"
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
+
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
 echo "script started executing at $TIMESTAMP" &>> $LOGFILE
-VALIDATE(){
+ 
+ VALIDATE(){
     if [ $1 -ne 0 ] 
     then 
         echo -e $2 .... $R Failed $N"
