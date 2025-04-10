46cfc19cd08e0854ab09c566e7c333d480506c15
KMNRao <kmohanarao09@gmail.com>
2025-04-10 11:35:11 +0530

    commit

--- 05-mysqlinstall.sh
+++ 05-mysqlinstall.sh
@@ -5,7 +5,20 @@ ID=$(id -u)
 if [ ID -ne 0 ] 
 then 
     echo "Error: please run this root user"
+    exit 1 # you can give other than 0
 else
     echo "you are root user"
 
 fi
+
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Error:: mysql installation was failed"
+    exit 1
+
+else
+   echo "mysql installing success"
+
+ fi  
